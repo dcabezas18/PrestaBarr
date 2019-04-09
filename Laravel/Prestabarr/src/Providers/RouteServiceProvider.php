26399cbb38850cded4011cda1b123b45d1775d37
dcabezas18@gmail.com <#QwEr1234>
2019-04-09 09:46:16 +0200

    Update api routes.

--- Laravel/Prestabarr/src/Providers/RouteServiceProvider.php
+++ Laravel/Prestabarr/src/Providers/RouteServiceProvider.php
@@ -23,8 +23,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
         parent::boot();
     }
 
@@ -36,24 +34,6 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-
-        //
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
     }
 
     /**
@@ -68,6 +48,12 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
              ->middleware('api')
              ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+            ->group(function () {
+                Route::post('/dolibarrStock', 'ApiController@dolibarrStock');
+                Route::post('/dolibarrPrice', 'ApiController@dolibarrPrice');
+                Route::post('/dolibarrProduct', 'ApiController@dolibarrProduct');
+                Route::post('/prestashopUpdateOrder', 'ApiController@prestashopUpdateOrder');
+                Route::post('/prestashopUpdateProduct', 'ApiController@prestashopUpdateProduct');
+            });
     }
 }
